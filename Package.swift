97beadb3294ff9e95a2cc1e2d3b60052cd025d3a
Armin Briegel <ab@scriptingosx.com>
2019-06-27 22:41:48 +0200

    setup Package.swift

--- Package.swift
+++ Package.swift
@@ -5,9 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "plaid",
+    platforms: [
+        .macOS(.v10_12),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.4.0")
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
